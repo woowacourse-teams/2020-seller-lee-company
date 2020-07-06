1a5901f2790cea12603658db8094e3f796aeea63
Yoonseo Nam <turtlenam0928@gmail.com>
2020-07-06 17:54:23 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## seller-lee-company
---
+++ front/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Seller Lee!</Text>
+    </View>
+  );
+}
