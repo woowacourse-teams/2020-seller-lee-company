2029f4b79aa2d7693d11fe75b540e7c9fdd4ee97
Gyeong-Jun Kim <lastpuzly@gmail.com>
2020-08-14 00:18:21 +0900

    feature: sql문 수정 (#117)
    * feat: sql문 수정

--- back/src/main/resources/db/migration/V1__Init.sql
+++ back/src/main/resources/db/migration/V1__Init.sql
@@ -4,6 +4,15 @@ drop table if exists member CASCADE;
 drop table if exists photo CASCADE;
 drop table if exists tag CASCADE;
 
+create table if not exists member
+(
+    member_id bigint auto_increment
+        primary key,
+    email varchar(255) not null,
+    password varchar(255) not null,
+    score double null
+);
+
 create table if not exists article
 (
     article_id bigint auto_increment
@@ -12,55 +21,30 @@ create table if not exists article
     contents longtext null,
     price bigint null,
     title varchar(255) null,
-    member_id bigint null
-)
-    engine=InnoDB;
-
-create index FK6l9vkfd5ixw8o8kph5rj1k7gu
-    on article (member_id);
+    member_id bigint null,
+    foreign key (member_id) references member(member_id) on update CASCADE
+);
 
 create table if not exists favorite
 (
     favorite_id bigint auto_increment
         primary key,
     article_id bigint null,
-    member_id bigint null
-)
-    engine=InnoDB;
-
-create index FK5w3q9ljpthkixo71hetx3ired
-    on favorite (member_id);
-
-create index FK9cgi9q9dxmh33ee1ndsy9w332
-    on favorite (article_id);
-
-create table if not exists member
-(
-    member_id bigint auto_increment
-        primary key,
-    email varchar(255) not null,
-    password varchar(255) not null,
-    score double null
-)
-    engine=InnoDB;
+    member_id bigint null,
+    foreign key (article_id) references article(article_id) on update CASCADE,
+    foreign key (member_id) references member(member_id) on update CASCADE
+);
 
 create table if not exists photo
 (
     article_id bigint not null,
-    photos varchar(255) null
-)
-    engine=InnoDB;
-
-create index FKrm4wnmb1hcwhxvrmf2x6gf9ox
-    on photo (article_id);
+    photos varchar(255) null,
+    foreign key (article_id) references article(article_id) on update CASCADE
+);
 
 create table if not exists tag
 (
     article_id bigint not null,
-    name varchar(255) null
-)
-    engine=InnoDB;
-
-create index FKhutavxkklg0b3kj7rmcnc9hvf
-    on tag (article_id);
-
+    name varchar(255) null,
+    foreign key (article_id) references article(article_id) on update CASCADE
+);
--- back/src/main/resources/db/migration/V2.2__Create_Chat_Room.sql
+++ back/src/main/resources/db/migration/V2.2__Create_Chat_Room.sql
@@ -8,6 +8,5 @@ create table if not exists chat_room
     member_id bigint null,
     foreign key (article_id) references article(article_id) on update CASCADE,
     foreign key (member_id) references member(member_id) on update CASCADE
-)
-    engine=InnoDB;
+);
 
