47fe91eb91ad3be5a604e9c77303051a0e560f5a
또링 <jnsorn@gmail.com>
2020-08-27 13:47:19 +0900

    refactor : [채팅] UI/UX 리팩토링 (#232)

--- front/src/screens/ChatScreen.tsx
+++ front/src/screens/ChatScreen.tsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, StyleSheet, View } from "react-native";
-import { GiftedChat, Send } from "react-native-gifted-chat";
+import {
+  ActivityIndicator,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { Bubble, GiftedChat, Send } from "react-native-gifted-chat";
 import { IconButton } from "react-native-paper";
 import { useRecoilValue } from "recoil/dist";
 import Fire from "../components/Chat/Fire";
@@ -9,7 +17,7 @@ import { memberAvatarState, memberNicknameState } from "../states/memberState";
 
 export default function ChatScreen() {
   const [messages, setMessages] = useState([]);
-  const memberNickname = useRecoilValue(memberNicknameState);
+  const memberNickname: string = useRecoilValue(memberNicknameState);
   const memberAvatar = useRecoilValue(memberAvatarState);
 
   useEffect(() => {
@@ -38,32 +46,70 @@ export default function ChatScreen() {
     </View>
   );
 
+  const renderBubble = (props: any) => {
+    return (
+      <View>
+        {memberNickname === props.currentMessage.user.name ? undefined : (
+          <Text style={styles.userName}>{props.currentMessage.user.name}</Text>
+        )}
+        <Bubble
+          {...props}
+          containerStyle={styles.bubbleContainer}
+          wrapperStyle={{
+            left: {
+              backgroundColor: "lightgrey",
+            },
+            right: {
+              backgroundColor: colors.primary,
+            },
+          }}
+          timeTextStyle={{
+            left: {
+              color: "#000",
+            },
+            right: {
+              color: "#ffffff",
+            },
+          }}
+        />
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <GiftedChat
-        messages={messages}
-        placeholder={"메세지를 입력해주세요."}
-        onSend={Fire.shared.send}
-        showUserAvatar
-        alwaysShowSend
-        user={{
-          name: memberNickname,
-          _id: memberNickname,
-          avatar: memberAvatar,
-        }}
-        renderSend={renderSend}
-        scrollToBottom
-        renderLoading={renderLoading}
-        renderUsernameOnMessage={true}
-        dateFormat="YYYY년 MM월 DD일"
-        timeFormat="HH:mm"
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.chatContainer}>
+        <GiftedChat
+          messages={messages}
+          placeholder={"메세지를 입력해주세요."}
+          onSend={Fire.shared.send}
+          showUserAvatar
+          alwaysShowSend
+          user={{
+            name: memberNickname,
+            _id: memberNickname,
+            avatar: memberAvatar,
+          }}
+          renderSend={renderSend}
+          renderLoading={renderLoading}
+          renderBubble={renderBubble}
+          renderAvatarOnTop={true}
+          dateFormat="YYYY년 MM월 DD일"
+          scrollToBottom
+          parsePatterns={(linkStyle) => [{ type: "phone", style: linkStyle }]}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "ios" ? 0 : StatusBar.currentHeight,
+  },
+  chatContainer: {
     flex: 1,
     justifyContent: "center",
   },
@@ -75,5 +121,21 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  bubbleContainer: {
+    flex: 1,
+  },
+  userName: {
+    flex: 1,
+    padding: 7,
+  },
+  emptySpace: {
+    flex: 1,
+  },
+  profileSpace: { width: 10 },
+  avatar: {},
+  userInfoContainer: {
+    width: 50,
   },
 });
